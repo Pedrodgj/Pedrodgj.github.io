734342e96cd1d1c3a9285591a093b54cfd82cab3
Pedro Galindez <82423879+Pedrodgj@users.noreply.github.com>
2026-05-23 11:13:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pasapalabras/' // 👈 nombre exacto del repo
+  base: '/' // 👈 nombre exacto del repo
 })
 
